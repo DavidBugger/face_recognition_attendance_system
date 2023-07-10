cb7529cfab7e1705f11c015dda334935f8b96c91
Gabriel Fatima <Gabriel.Fatima@accessng.local>
2023-07-10 11:45:08 +0100

    First commit

---
+++ Home_Page.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from datetime import datetime
+import pandas as pd
+
+st.set_page_config(
+    page_title="Attendance System", page_icon="📊", layout="wide"
+)
+st.image("Dataset/Messi.jpg")
+
